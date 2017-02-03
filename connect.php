7e37c7127c134a9e724a006dd047f802495e731c
Aditya Janata Priya <adityajanata@ymail.com>
2017-02-03 15:25:07 +0700

    update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "ap-cdbr-azure-southeast-b.cloudapp.net";
-$user = "b5c0ef4301762f";
-$pass = "39fd9eef";
-$name = "webapp_db";
+$user = "b9ff1a1fd97e56";
+$pass = "ca8fe94c";
+$name = "websql_db";
  
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
